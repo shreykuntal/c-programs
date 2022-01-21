163f86dd83f03c4409339ee9d409df9008cbcfe2
Shreyansh Kuntal <45984673+shreykuntal@users.noreply.github.com>
2022-01-21 22:23:53 +0530

    Add files via upload

---
+++ long digit prime checker + time taken/mycprogram.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+int main()
+{
+    int is_prime = 1;
+    unsigned long long int  num;
+    printf("Enter a number: ");
+    clock_t begin = clock();
+    scanf("%llu",&num);
+    unsigned long long int  sqrt_num;
+    sqrt_num = sqrt(num);
+    unsigned long long int num_int = (unsigned long long int)num;
+    unsigned long long int sqrt_num_int = (unsigned long long int)sqrt_num;
+    unsigned long long int i;
+    for (i=2; i<=sqrt_num_int; i++)
+    {
+        if (num_int%i==0) {
+            is_prime = 0;
+            break;
+        }
+    }
+    clock_t end = clock();
+    if (is_prime == 0){
+        printf("Not a prime!\n");
+        printf("Least divisor: %llu\n",i);
+    }else {
+        printf("Yes Prime!\n");
+    }
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time taken: %lf seconds\n", time_spent);
+    return 0;
+}
